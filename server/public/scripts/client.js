c5e9d0c57b5359b3a360f024746fd2fa990da194
keef hassan <keefhassan4@gmail.comgit config --global user.email keefhassan4@gmail.coome>
2024-02-03 23:42:20 -0600

    create client functions for  submitting todo and get todos

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,56 @@
 console.log('JS is sourced!');
+//client.js steps
+// 1 - create a function for submitting todo to the server
+//2 - create a function fot getting all todo & shpw on the table
+// 3 - create a functions for mark as complete & delete.
+
+
+
+
+
+function submitItem(event) {
+    event.preventDefault()
+    const input = document.getElementById("toDoTextInput")
+    const newTodo = {
+        text: input.value
+    }
+    axios({
+        method: 'POST',
+        url: '/todos',
+        data: newTodo
+    }).then(response => {
+        getTodos();
+
+    }).catch(error => {
+        console.log('Error in POST request', error);
+    });
+}
+
+function getTodos() {
+    axios({
+        method: 'GET',
+        url: '/todos'
+    }).then(response => {
+        let tbody = document.getElementsById("todobody")
+        tbody.innerHTML = '';
+        for (let todo of response.data) {
+
+            tbody.innerHTML += `
+      <tr>
+        <td>${todo.text}</td>
+        <td><button onclick="MarkAsComplete(${todo.id})">Mark As Complete</button></td>
+        <td><button onclick="deleteTodo(${todo.id})">Delete</button></td>
+      </tr>
+    `;
+        }
+
+
+        console.log('GET response is good', response);
+    }).catch(error => {
+        console.log('GET response went wrong', error);
+    });
+}
+function MarkAsComplete (){
+
+}
+function deleteTodo(){}
